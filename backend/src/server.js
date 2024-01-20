1f73ce9bb021fe82a045530310bd4d68cbdc8276
saJeremyQin <102841987+saJeremyQin@users.noreply.github.com>
2024-01-20 01:31:25 +0000

    verify with contentful, read dishType

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,36 @@
 import express from "express";
+import { createClient } from 'contentful';
+
 
 const app = express();
 
-app.get('/hello',(req, res) => {
-    res.send('Hello world');
+const client = createClient({
+    space: 'xcgzqirx0bln',
+    environment: 'develop',
+    accessToken: 'zu0M1zS19jfMeAroc1tARgWxD03jd8tWdG5nBtUJt9U',
+})
+
+
+app.get('/api/dishtypes', async (req, res) => {
+
+    try {
+        const response = await client.getEntries({
+            content_type: 'dishType'
+        });
+
+        // Extract the items
+        const dishtypes = response.items.map((item) => ({
+            id: item.sys.id,
+            name: item.fields.name,
+        }));
+        // const dishtypes = response.items;
+        res.send(dishtypes);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({
+            error:'Internal server error.'
+        })
+    }
 })
 
 app.listen('8000', () => {
