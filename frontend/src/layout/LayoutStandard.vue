f415aa36ff8d222e448811a3039160651576e76c
saJeremyQin <343220868@qq.com>
2024-01-26 13:06:34 +1030

    layout works, with PageHome

--- frontend/src/layout/LayoutStandard.vue
+++ frontend/src/layout/LayoutStandard.vue
@@ -1,18 +1,17 @@
 <template>
     <v-layout class="rounded rounded-md">
-      <v-app-bar title="Application bar"></v-app-bar>
-  
-      <v-navigation-drawer>
-        <v-list>
-          <v-list-item title="Navigation drawer"></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-  
-      <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
-        Main Content
-      </v-main>
+      <header-bar />
+      <v-container>
+        <nav-drawer />
+        <main-section>
+          <router-view />
+        </main-section>
+      </v-container>
     </v-layout>
 </template>
 
 <script setup>
+import HeaderBar from './HeaderBar.vue';
+import NavDrawer from './NavDrawer.vue';
+import MainSection from './MainSection.vue';
 </script>
---
+++ frontend/src/layout/MainSection.vue
@@ -0,0 +1,5 @@
+<template>
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+        <slot />
+    </v-main>
+</template>
