5a0ced910a80f6b4cac270c57fab2d235c16e450
saJeremyQin <343220868@qq.com>
2024-01-29 00:21:52 +1030

    update layout

--- frontend/src/layout/LayoutLogin.vue
+++ frontend/src/layout/LayoutLogin.vue
@@ -1,8 +1,17 @@
 <template>
-    <div>
+    <div class="loginWrapper">
         <router-view />
     </div>
 </template>
 
 <script setup>
 </script>
+
+<style lang="scss" scoped>
+.loginWrapper {
+	width: 100vw;
+	height: 100vh;
+	background-image: url('../assets/login_bg.jpg');
+	background-size: cover;
+}
+</style>
--- frontend/src/pages/PageLogin.vue
+++ frontend/src/pages/PageLogin.vue
@@ -1,157 +1,104 @@
 <template>
-	<main class="login">
-		<section class="forms">
-			<form class="register" @submit.prevent="register">
-				<h2>Register</h2>
-				<input 
-					type="email" 
-					placeholder="Email address"
-					v-model="register_form.email" />
-				<input 
-					type="password" 
-					placeholder="Password" 
-					v-model="register_form.password" />
-				<input 
-					type="submit" 
-					value="Register" />
-			</form>
-
-			<form class="login" @submit.prevent="login">
-				<h2>Login</h2>
-				<input 
-					type="email" 
-					placeholder="Email address"
-					v-model="login_form.email" />
-				<input 
-					type="password" 
-					placeholder="Password" 
-					v-model="login_form.password" />
-				<input 
-					type="submit" 
-					value="Login" />
-			</form>
-
-		</section>
-	</main>
+	<v-container fluid="true">
+		<v-row>
+			<v-col>
+				<v-img :src="logoPic" class="logo-img"></v-img>
+			</v-col>
+		</v-row>
+		<v-row>
+			<v-col class="form-col">
+				<v-form @submit.prevent class="form">
+					<v-text-field
+						v-model="email"
+						:rules="rules"
+						type="email"
+						class="form-input"
+					>
+						<template #label>
+							<span class="label-text">Email address</span>
+						</template>
+					</v-text-field>
+					<v-text-field 
+						v-model="password"
+						:rules="rules"
+						type="password"
+						label="password"
+						class="form-input"
+					>
+						<template #label>
+							<span class="label-text">Password</span>
+						</template>
+					</v-text-field>
+					<v-btn type="submit" block class="mt-2 submit-btn">
+						Submit
+					</v-btn>
+				</v-form>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
-<script>
-import { ref } from 'vue'
-// import { useStore } from 'vuex'
+<script setup>
+import logoPic from '@/assets/icon.png';
+</script>
 
-export default {
-	setup () {
-		const login_form = ref({});
-		const register_form = ref({});
-		// const store = useStore();
+<style lang="scss" scoped>
 
-		const login = () => {
-			// store.dispatch('login', login_form.value);
-		}
+.logo-img {
+	width: 64px;
+	height: 64px;
+	margin-left: 32px;
+}
 
-		const register = () => {
-			// store.dispatch('register', register_form.value);
-		}
+.form-col {
+	display: flex;
+	justify-content: center;
+	// align-items: flex-start;
+	position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+.form {
+	width: 400px;
+	height: 320px;
 
-		return {
-			login_form,
-			register_form,
-			login,
-			register
-		}
+	.label-text {
+		color:white;
+	}
+
+	:deep(.v-input__control)::before {
+		content: '';
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		height: 2px;
+		border-bottom-color: #c77952;
+	}
+
+	:deep(.v-input__control):focus-within::before {
+		border-bottom-color: #e9983e;
 	}
 }
-</script>
-<style lang="scss" scoped>
-@use "@/styles/base" as *;
 
-.forms {
-	display: flex;
-	min-height: 100vh;
-}
-
-form {
-	flex: 1 1 0%;
-	padding: 8rem 1rem 1rem;
-}
-
-form.register {
-	// color: #FFF;
-	color: $colour-blue;
-	background-color: rgb(245, 66, 101);
-	background-image: linear-gradient(
-		to bottom right,
-		rgb(245, 66, 101) 0%,
-		rgb(189, 28, 60) 100%
-	);
-}
-
-h2 {
-	font-size: 2rem;
-	text-transform: uppercase;
-	text-align: center;
-	margin-bottom: 2rem;
-}
-
-input {
-	appearance: none;
-	border: none;
-	outline: none;
-	background: none;
-
-	display: block;
+.submit-btn {
 	width: 100%;
-	max-width: 400px;
-	margin: 0 auto;
-	font-size: 1.5rem;
-	margin-bottom: 2rem;
-	padding: 0.5rem 0rem;
+	height: 100%;
+	padding-top: 13px;
+	padding-bottom: 13px;
+	border-radius: 30px;
+	display: inline-flex;
+	justify-content: center;
+	align-items: center;
+	background: linear-gradient(136deg, #e9983e 0%, #c77952 100%);
+	text-align: center;
+	color: white;
+	font-size: 16px;
+	font-family: 'SF Pro Display', sans-serif;
+	font-weight: 500;
+	line-height: 24px;
+	word-wrap: break-word;
 }
 
-input:not([type="submit"]) {
-	opacity: 0.8;
-	transition: 0.4s;
-}
-
-input:focus:not([type="submit"]) {
-	opacity: 1;
-}
-
-input::placeholder {
-	color: inherit;
-}
-
-form.register input:not([type="submit"]) {
-	color: #FFF;
-	border-bottom: 2px solid #FFF;
-}
-
-form.login input:not([type="submit"]) {
-	color: #2c3e50;
-	border-bottom: 2px solid #2c3e50;
-}
-
-form.login h2 {
-  color: rgb(245, 66, 101); ;
-}
-
-form.login input[type="submit"] {
-	background-color: rgb(245, 66, 101);
-	color: #FFF;
-	font-weight: 700;
-	padding: 1rem 2rem;
-	border-radius: 0.5rem;
-	cursor: pointer;
-	text-transform: uppercase;
-}
-
-form.register input[type="submit"] {
-	background-color: #FFF;
-	color: rgb(245, 66, 101);
-	font-weight: 700;
-	padding: 1rem 2rem;
-	border-radius: 0.5rem;
-	cursor: pointer;
-	text-transform: uppercase;
-}
 </style>
