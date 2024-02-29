397dea3f9df3ccc39d4f753b2cfea81d536bed06
Jeremy Qin <102841987+saJeremyQin@users.noreply.github.com>
2024-02-29 06:50:47 +0000

    remove uncomment of cdatoken/cmatoken

--- backend/src/contentfulClient.js
+++ backend/src/contentfulClient.js
@@ -20,12 +20,10 @@ export const cdaClient = createCDA({
     space: spaceId,
     environment: environmentId,
     accessToken: process.env.CDA_TOKEN,
-    // accessToken:'zu0M1zS19jfMeAroc1tARgWxD03jd8tWdG5nBtUJt9U',
 });
 
 export const cmaClient = createCMA({
     space: spaceId,
     environment: environmentId,
     accessToken: process.env.CMA_TOKEN,
-    // accessToken:'CFPAT-Um3vfnH5eISh-24r-RNgbKaRcsUBuX0CZ_FFG6IJ7Is',
 });
