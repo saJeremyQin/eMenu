65fcd6887b9735c910ce8ffd2b95fa09f7d3c725
saJeremyQin <343220868@qq.com>
2024-02-29 12:20:22 +1030

    firebase ENV variables on

--- backend/src/authMiddleware.js
+++ backend/src/authMiddleware.js
@@ -1,14 +1,21 @@
 import admin from 'firebase-admin';
 // import serviceAccount from "../eMenuAccountKey.json";
 
+let firebaseKey;
+if(process.env.PRIVATE_KEY_ID) {
+  firebaseKey = {
+    "private_key_id": process.env.PRIVATE_KEY_ID,
+    "private_key": process.env.PRIVATE_KEY.replace(/\\n/gm, "\n"),
+    "project_id": process.env.PROJECT_ID,
+    "client_email": process.env.CLIENT_EMAIL
+  };
+} else {
+  const serviceAccount = require("../eMenuAccountKey.json");
+  firebaseKey = serviceAccount;
+}
 admin.initializeApp({
     // credential: admin.credential.cert(serviceAccount),
-    credential: admin.credential.cert({
-        "private_key_id": process.env.PRIVATE_KEY_ID,
-        "private_key": process.env.PRIVATE_KEY.replace(/\\n/gm, "\n"),
-        "project_id": process.env.PROJECT_ID,
-        "client_email": process.env.CLIENT_EMAIL
-    }),
+    credential: admin.credential.cert(firebaseKey),
     databaseURL: "https://emenu-41e86.firebaseio.com"
 });
 
