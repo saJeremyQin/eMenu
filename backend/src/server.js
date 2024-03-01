101df35d8a1aeca9773469a40938833f130c7c05
Jeremy Qin <102841987+saJeremyQin@users.noreply.github.com>
2024-03-01 01:25:47 +0000

    update setHeader for CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,14 @@ import dishtypeRoutes from './routes/dishtypeRoutes';
 
 const app = express();
 
+// set CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://vigilant-palm-tree-577qwrxjwrgcvjjw-3000.preview.app.github.dev');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); 
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
+    next();
+});
+
 app.use(cors());
 app.use(express.static(path.resolve(__dirname,"../dist"),{ maxAge:"1y", etag:false}));
 
