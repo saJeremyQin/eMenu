96c65658e8c397b27e39ac139ccf8ddd9508d4ae
Jeremy Qin <102841987+saJeremyQin@users.noreply.github.com>
2024-03-01 00:07:09 +0000

    deploy fullstack site correctly, except login page apperance

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,26 @@
 import express from "express";
 import cors from 'cors';
+import path from 'path';
 import authenticateUser from "./authMiddleware";
 import userRoutes from './routes/userRoutes';
 import dishtypeRoutes from './routes/dishtypeRoutes';
 
-
-
 const app = express();
 
 app.use(cors());
+app.use(express.static(path.resolve(__dirname,"../dist"),{ maxAge:"1y", etag:false}));
+
 app.use(express.json());
 app.use(authenticateUser);
 
 app.use('/api/users', userRoutes); // Mounting user routes
 app.use('/api/dishtypes', dishtypeRoutes);
 
-app.listen('8000', () => {
-    console.log("Server is listening on Port 8000");
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../dist/index.html"));
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("Server is listening on Port " + port);
 })
