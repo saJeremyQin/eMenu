48d9799a0adeba4661f47effc18e435ebb0605d7
saJeremyQin <343220868@qq.com>
2024-03-02 18:49:33 +1030

    update corsOptions and check continueurl

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,15 +8,14 @@ import dishtypeRoutes from './routes/dishtypeRoutes';
 const app = express();
 
 // set CORS
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://emenu-el2v.onrender.com');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); 
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
-    next();
-});
+const corsOptions = {
+  origin: ['https://emenu-el2v.onrender.com', 'https://vigilant-palm-tree-577qwrxjwrgcvjjw-8080.preview.app.github.dev/'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
 
 // use CORS
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.static(path.resolve(__dirname,"../dist"),{ maxAge:"1y", etag:false}));
 
 app.use(express.json());
