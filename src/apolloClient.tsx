def4fdb5fe50f4e54adf2bd46eeff4e6890e0309
saJeremyQin <343220868@qq.com>
2025-01-09 21:26:28 +1030

    local server connected, no issue

--- src/apolloClient.tsx
+++ src/apolloClient.tsx
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "https://whats-menu-server.vercel.app/api",
+    // uri: "https://whats-menu-server.vercel.app/api",
+    uri: "http://localhost:5005/api",
     cache: new InMemoryCache(),
 });
 
--- src/graphql/query.tsx
+++ src/graphql/query.tsx
@@ -3,6 +3,7 @@ import { gql } from '@apollo/client';
 export const GET_DISHES = gql`
     query GetDishes {
         dishes {
+            id
             name
             type
             price
